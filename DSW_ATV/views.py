f1c046ba490b28dca0859e0baf1e2574a6743979
RIAN CARLOS VERAS DE SOUSA <20221094040017@ifrn.local>
2025-02-06 16:01:32 -0300

    alterado todas as views

--- DSW_ATV/views.py
+++ DSW_ATV/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import QuestionForm, FornecedorForm, CategoriaForm, ProdutoForm
-from .models import *
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Produto, Categoria, Fornecedor
 
 # CBV teste
-
-from django.views.generic import ListView
-from .models import Produto, Categoria, Fornecedor
 
 class ProdutoListView(ListView):
     model = Produto
@@ -14,7 +13,7 @@ class ProdutoListView(ListView):
 
 class CategoriaListView(ListView):
     model = Categoria
-    template_name = "produtos/listar_categoria"
+    template_name = "produtos/listar_categorias.html"
     context_object_name = "categorias"
 
 class FornecedorListView(ListView):
@@ -22,40 +21,25 @@ class FornecedorListView(ListView):
     template_name = "produtos/listar_fornecedores.html"
     context_object_name = "fornecedores"
 
-def detalhes(request, pk):
-    detalhes_produto = get_object_or_404(Produto, id=pk)
-    context = {
-        'produtos': detalhes_produto 
-    }
-    return render(request, 'detalhes_produtos.html', context)
+class ProdutoDetailView(DetailView):
+    model = Produto
+    template_name = 'detalhes_produtos.html'
+    context_object_name = 'produtos'
 
-def cadastrar_categoria(request):
-    if request.method == 'POST':
-        form = CategoriaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_categorias')
-    else:
-        form = CategoriaForm()
-    return render(request, 'cadastrar_categoria.html', {'form': form})
+class CategoriaCreateView(CreateView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = 'cadastrar_categoria.html'
+    success_url = reverse_lazy('listar_categorias')
 
-def cadastrar_fornecedor(request):
-    if request.method == 'POST':
-        form = FornecedorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_fornecedores')
-    else:
-        form = FornecedorForm()
+class FornecedorCreateView(CreateView):
+    model = Fornecedor
+    form_class = FornecedorForm
+    template_name = 'cadastrar_fornecedor.html'
+    success_url = reverse_lazy('listar_fornecedores')
 
-    return render(request, 'cadastrar_fornecedor.html', {'form':form})
-
-def cadastrar_produtos(request):
-    if request.method == 'POST':
-        form = ProdutoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = ProdutoForm()
-    return render(request, 'cadastrar_produto.html', {'form': form})
+class ProdutoCreateView(CreateView):
+    model = Produto
+    form_class = ProdutoForm
+    template_name = 'cadastrar_produto.html'
+    success_url = reverse_lazy('index')
