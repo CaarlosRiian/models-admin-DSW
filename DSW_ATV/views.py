091aaa27dc2a5fbf04d676f1d58e88a998e339e8
Rian Carlos <101999658+CaarlosRiian@users.noreply.github.com>
2025-02-06 21:38:14 -0300

    filtragem e paginação adicionada!

--- DSW_ATV/views.py
+++ DSW_ATV/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import QuestionForm, FornecedorForm, CategoriaForm, ProdutoForm
+from django.shortcuts import render, get_object_or_404
+from .forms import FornecedorForm, CategoriaForm, ProdutoForm
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator
 from .models import Produto, Categoria, Fornecedor
+from django.db.models import Q
 
 # CBV teste
 
 class ProdutoListView(ListView):
     model = Produto
     template_name = "produtos/index.html"
-    context_object_name = "produto"
+    context_object_name = "produtos"
+    paginate_by = 6
+
+    def get_queryset(self):
+        queryset = Produto.objects.all()
+        nome_busca = self.request.GET.get('nome', '')
+        preco_min = self.request.GET.get('preco_min', '')
+        preco_max = self.request.GET.get('preco_max', '')
+
+        if nome_busca:
+            queryset = queryset.filter(nome_produto__icontains=nome_busca) 
+
+        if preco_min:
+            queryset = queryset.filter(preco__gte=preco_min)
+        if preco_max:
+            queryset = queryset.filter(preco__lte=preco_max)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nome'] = self.request.GET.get('nome', '')
+        context['preco_min'] = self.request.GET.get('preco_min', '')
+        context['preco_max'] = self.request.GET.get('preco_max', '')
+        return context
 
 class CategoriaListView(ListView):
     model = Categoria
